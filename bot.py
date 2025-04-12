fd3fd3a83f1006e938c3b806959d62737ceba7f9
Luzanow <Tempsupor@gmail.com>
2025-04-12 22:33:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = "7680848123:AAHqNizmx3hOXmjVOmzdnwQCenlXHTWX8OA"
-ADMIN_CHAT_ID = '5498505652'
+API_TOKEN = "7680848123:AAHqNizmx3hOXmjVOmzdnwQCenlXHTWX8OA"  # Ваш токен
+ADMIN_CHAT_ID = '5498505652'  # Заміни на свій Telegram ID
+
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
